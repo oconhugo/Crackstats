06db80f0e0e9b7126ef9f4eaaadfaf0cd1cf9d3c
oconhugo <hugo.ocon@live.com>
2020-12-07 23:08:56 -0700

    Add files via upload

--- php/insert_into_db.php
+++ php/insert_into_db.php
@@ -8,13 +8,22 @@ include "dbconfig.php";
     $dob = mysqli_real_escape_string($conn, $_POST['dob']);
     $gender = mysqli_real_escape_string($conn, $_POST['gender']);
     $password = mysqli_real_escape_string($conn, $_POST['password']);
- 
-        $query = "INSERT INTO t_register_users (First_Name, Last_Name, dob, Gender, Salt, Email, Password)
-  			  VALUES('$name', '$last_name', '$DOB', '$gender', '', '$email', '$password')";
-    $results = mysqli_query($conn, $query);
-    if($results>0)
-    {
-        echo "user added successfully";
-    }
     
+    $query_check_existance = "SELECT EXISTS(SELECT * from t_register_users WHERE Email='$email')";
+
+    $query = "INSERT INTO t_register_users (First_Name, Last_Name, dob, Gender, Salt, Email, Password)
+  			  VALUES('$name', '$last_name', '$DOB', '$gender', '', '$email', '$password')";
+    
+    $results_existance = mysqli_query($conn, $query_check_existance);
+	$user_exist = mysqli_fetch_row($results_existance);
+	if($user_exist[0] == 0)
+    {
+        $results = mysqli_query($conn, $query);
+        if($results>0)
+        {
+            echo "user added successfully";
+        }
+    } else{
+        echo "user already registered";
+    }
 ?>
