ae6919cf0c1b1f2269eb6cc2ea6185d63f2c3de2
AlexIbarra04 <63935654+AlexIbarra04@users.noreply.github.com>
2020-12-09 22:31:26 -0500

    Add files via upload

--- php/login_verify_user.php
+++ php/login_verify_user.php
@@ -6,13 +6,22 @@ include "dbconfig.php";
     $password = mysqli_real_escape_string($conn, $_POST['password']);
  
     $query = "SELECT Email, Password FROM t_register_users WHERE Email = '$email' AND Password = '$password'";
-    $results = mysqli_query($conn, $query);
-    if($results> 0)
+    $results_user_exists = mysqli_query($conn, $query);
+	
+	$query_get_info = "SELECT * FROM t_register_users WHERE Email = '$email' AND Password = '$password'";
+	$results_get_info = mysqli_query($conn, $query_get_info);
+	$number_rows_returned_info = mysqli_num_rows($results_get_info);
+	
+    if($results_user_exists> 0)
     {
-        while($row = $results->fetch_assoc()){
-             echo "User exist in database";
-             //echo "password: " . $row['Password'];
-        }
+		if($number_rows_returned_info==1){
+			$returned_user = $results_get_info->fetch_assoc();
+			echo json_encode($returned_user);
+		}
+        //while($row = $results->fetch_assoc()){
+        //    echo "User exist in database";
+        //    //echo "password: " . $row['Password'];
+        //}
     }
     
 ?>
