ecf30d2d34cefd82d589382bdcd8ba3c444827e8
oconhugo <hugo.ocon@live.com>
2021-01-15 17:19:54 -0700

    Add files via upload

--- php/send_player_request.php
+++ php/send_player_request.php
@@ -1,6 +1,6 @@
 <?php 
 include "dbconfig.php";
-// send team request
+// send new player request
   
     $sender = mysqli_real_escape_string($conn, $_POST['sender']);
     $team = mysqli_real_escape_string($conn, $_POST['team']);
@@ -8,11 +8,13 @@ include "dbconfig.php";
     $comment = mysqli_real_escape_string($conn, $_POST['comment']);
     
     $query = "INSERT INTO t_notifications (Message, Sender, Receiver, Type_Of_Request, Team, League)
-  			  VALUES ('$comment', '$sender', (SELECT Administrator FROM t_leagues where Name = '$league'), 1, '$team', '$league')";
+  			  VALUES ('$comment', '$sender', (SELECT Owner FROM t_teams where League = '$league' and Name = '$team'), 2, '$team', '$league')";
     
     $results = mysqli_query($conn, $query);
 	if($results>0)
     {
         echo "Request sended Succesfully";
+    }else{
+        echo "no entre";
     }
 ?>
